ce62f238db12186fb21ef8879604891b9eb94547
jihye0526 <samtafe97@naver.com>
2023-06-23 23:01:07 +0900

    2023.06.23 - 수정

--- 프로그래머스/Lv2/과제 진행하기(X).py
+++ 프로그래머스/Lv2/과제 진행하기(X).py
@@ -1,4 +1,4 @@
-# 내 풀이
+# 다른 사람 풀이 1
 def solution(plans):
     answer = []
     stack = []
@@ -34,6 +34,21 @@ def solution(plans):
     
     return answer
 
+# 다른 사람 풀이 2
+def solution2(plans):
+    plans = sorted(map(lambda x: [x[0], int(x[1][:2]) * 60 + int(x[1][3:]), int(x[2])], plans), key=lambda x: -x[1])
+
+    lst = []
+    while plans:
+        x = plans.pop()
+        for i, v in enumerate(lst):
+            if v[0] > x[1]:
+                lst[i][0] += x[2]
+        lst.append([x[1] + x[2], x[0]])
+    lst.sort()
+
+    return list(map(lambda x: x[1], lst))
+
 print(solution([["korean", "11:40", "30"], ["english", "12:10", "20"], ["math", "12:30", "40"]]	))
 print(solution([["science", "12:40", "50"], ["music", "12:20", "40"], ["history", "14:00", "30"], ["computer", "12:30", "100"]]	))
-print(solution([["aaa", "12:00", "20"], ["bbb", "12:10", "30"], ["ccc", "12:40", "10"]]	))
+print(solution2([["aaa", "12:00", "20"], ["bbb", "12:10", "30"], ["ccc", "12:40", "10"]]	))
