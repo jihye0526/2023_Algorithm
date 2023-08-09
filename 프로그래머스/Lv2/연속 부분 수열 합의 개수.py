b892efcc33756765d87bfef1d8cb0b75f212e5ed
jihye0526 <samtafe97@naver.com>
2023-08-09 22:01:14 +0900

    2023.08.09

---
+++ 프로그래머스/Lv2/연속 부분 수열 합의 개수.py
@@ -0,0 +1,26 @@
+# 내 풀이
+def solution(elements):
+    answer = []
+    double = elements * 2
+    
+    for i in range(1, len(elements)+1):
+        for j in range(len(double)):
+            answer.append(sum(double[j:j+i]))
+            
+    return len(set(answer))
+
+# 다른 사람 풀이
+def solution2(elements):
+    ll = len(elements)
+    res = set()
+
+    for i in range(ll):
+        ssum = elements[i]
+        res.add(ssum)
+        for j in range(i+1, i+ll):
+            ssum += elements[j%ll]
+            res.add(ssum)
+    return len(res)
+
+print(solution([7,9,1,1,4]))
+print(solution2([7,9,1,1,4]))
