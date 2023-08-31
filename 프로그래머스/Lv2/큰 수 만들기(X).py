69123f99d908aed34bf022f27567013f9fb561bc
jihye0526 <samtafe97@naver.com>
2023-08-31 22:22:02 +0900

    2023.08.31

---
+++ 프로그래머스/Lv2/큰 수 만들기(X).py
@@ -0,0 +1,44 @@
+# 다른 사람 풀이 1
+def solution(number, k):
+    answer = [] # Stack
+    
+    for num in number:
+        if not answer:
+            answer.append(num)
+            continue
+        if k > 0:
+            while answer[-1] < num:
+                answer.pop()
+                k -= 1
+                if not answer or k <= 0:
+                    break
+        answer.append(num)
+        
+    answer = answer[:-k] if k > 0 else answer
+    return ''.join(answer)
+
+# 다른 사람 풀이 2
+def solution2(number, k):
+    stack = [number[0]]
+    for num in number[1:]:
+        while len(stack) > 0 and stack[-1] < num and k > 0:
+            k -= 1
+            stack.pop()
+        stack.append(num)
+    if k != 0:
+        stack = stack[:-k]
+    return ''.join(stack)
+
+# 다른 사람 풀이 3
+def solution3(number, k):
+    st = []
+    for i in range(len(number)):
+        while st and k > 0 and st[-1] < number[i]:
+            st.pop()
+            k -= 1
+        st.append(number[i])
+    return ''.join(st[:len(st) - k])
+
+print(solution("1924", 2))
+print(solution2("1231234", 3))
+print(solution3("4177252841", 4))
--- 프로그래머스/Lv2/피로도(X).py
+++ 프로그래머스/Lv2/피로도(X).py
@@ -1,5 +1,23 @@
 # 다른 사람 풀이
+answer = 0
+
 def solution(k, dungeons):
+    global answer    
     v = [False] * len(dungeons)
+    dfs(k, 0, dungeons, v)
+    return answer
+
+def dfs(k, cnt, dungeons, v):
+    global answer
+    if cnt > answer:
+        answer = cnt
+    
+    for i, dungeon in enumerate(dungeons):
+        a, b = dungeon
+
+        if not v[i] and k >= a:
+            v[i] = True
+            dfs(k-b, cnt+1, dungeons, v)
+            v[i] = False
 
 print(solution(80, [[80,20],[50,40],[30,10]]))
