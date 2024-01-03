aefe32c03954b165bd481e67e35547983dcc9386
JIHYE <74289724+jihye0526@users.noreply.github.com>
2024-01-04 00:10:09 +0900

    Time: 36 ms (78.42%), Space: 17.4 MB (11.26%) - LeetHub

---
+++ LeetCode/Easy/1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.py
@@ -0,0 +1,9 @@
+class Solution:
+    def gcdOfStrings(self, str1: str, str2: str) -> str:
+        # 두 문자를 각각 더했을 때 결과가 다르다면 반복되지 않는 경우라는 것
+        if str1 + str2 != str2 + str1:
+            return ""
+
+        # 겹치는 문자의 길이는 무조건 두 문자의 길이의 최대공약수
+        length = math.gcd(len(str1), len(str2))
+        return str2[:length]
