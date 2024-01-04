426754f2f4884f6f46f2fa93611ace743f6f7162
jihye0526 <samtafe97@naver.com>
2024-01-04 21:00:45 +0900

    2024.01.04 - 다른 풀이 추가

--- LeetCode/Easy/1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.py
+++ LeetCode/Easy/1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.py
@@ -1,5 +1,6 @@
-class Solution:
-    def kidsWithCandies(self, candies: List[int], extraCandies: int) -> List[bool]:
+# 내 풀이 1
+class Solution1:
+    def kidsWithCandies1(self, candies: List[int], extraCandies: int) -> List[bool]:
         answer = []
         maxCandy = max(candies)
         
@@ -11,3 +12,24 @@ class Solution:
                 answer.append(False)
         
         return answer
+    
+# 내 풀이 2
+class Solution2:
+    def kidsWithCandies2(self, candies: List[int], extraCandies: int) -> List[bool]:
+        answer = []
+        maxCandy = max(candies)
+        
+        for i in range(len(candies)):
+            temp = candies[i] + extraCandies
+            if temp >= maxCandy:
+                answer.append(True)
+            else:
+                answer.append(False)
+        
+        return answer
+    
+# 다른 사람 풀이
+class Solution3:
+    def kidsWithCandies(self, candies: List[int], extraCandies: int) -> List[bool]:
+        maxCandies = max(candies)
+        return [candy+extraCandies >= maxCandies for candy in candies]
