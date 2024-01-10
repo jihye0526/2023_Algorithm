78e6c8509b32953094e6307acdbed06bcdb3453e
jihye0526 <samtafe97@naver.com>
2024-01-10 20:36:07 +0900

    멀쩡한 사각형 풀이방법 추가

--- 프로그래머스/Lv2/멀쩡한 사각형(X).js
+++ 프로그래머스/Lv2/멀쩡한 사각형(X).js
@@ -1,4 +1,4 @@
-// 다른 사람 풀이
+// 다른 사람 풀이 1
 function solution(w, h) {
     return w * h - (w + h - gcd(w, h));
 }
@@ -9,4 +9,17 @@ function gcd(a, b) {
     return gcd(b, a % b);
 }
 
+// 다른 사람 풀이 2
+function solution2(w,h){
+    const slope = h / w;
+    let result = 0;
+
+    for(let i = 1; i <= w; i++){
+        result += Math.ceil(slope * i);
+    }
+
+    return ((h * w) - result) * 2;
+}
+
 console.log(solution(8, 12));
+console.log(solution2(8, 12));
