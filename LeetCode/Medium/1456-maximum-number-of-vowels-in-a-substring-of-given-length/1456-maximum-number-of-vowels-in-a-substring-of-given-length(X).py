2659e780cd4256ddcf90c58eb5bdf56de3780af5
jihye0526 <samtafe97@naver.com>
2024-01-30 22:13:07 +0900

    2024.01.30 - LeetCode(1456)

--- LeetCode/Medium/1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length(X).py
+++ LeetCode/Medium/1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length(X).py
@@ -1,10 +1,8 @@
-import re
-
+# 다른 사람 풀이
 class Solution:
     def maxVowels(self, s: str, k: int) -> int:
         arr = [1 if i in 'aeiou' else 0 for i in s]
         curr = temp = sum(arr[:k])
-        aaa = arr[:k]
 
         for i in range(k, len(s)):
             temp += arr[i] - arr[i-k]
