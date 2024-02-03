cfb2ea8420f7342b1e2e284712be4654fac88ba0
jihye0526 <samtafe97@naver.com>
2024-02-03 16:35:51 +0900

    2024.02.03 - LeetCode(1493)

--- LeetCode/Medium/1493-longest-subarray-of-1s-after-deleting-one-element/1493-longest-subarray-of-1s-after-deleting-one-element.py
+++ LeetCode/Medium/1493-longest-subarray-of-1s-after-deleting-one-element/1493-longest-subarray-of-1s-after-deleting-one-element.py
@@ -1,3 +1,22 @@
+# 내 풀이
+class Solution:
+    def longestSubarray(self, nums: List[int]) -> int:
+        left = right = 0
+        k = 1
+
+        for right in range(len(nums)):
+            if nums[right] == 0:
+                k -= 1
+
+            if k < 0:
+                if nums[left] == 0:
+                    k += 1
+
+                left += 1
+
+        return right - left
+
+# 다른 사람 풀이
 class Solution:
     def longestSubarray(self, nums: List[int]) -> int:
         answer = 0
