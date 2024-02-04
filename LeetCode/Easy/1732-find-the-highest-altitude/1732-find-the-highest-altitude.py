51434a5dea929ac2183fa6e4c76a1ce0a5323e81
jihye0526 <samtafe97@naver.com>
2024-02-04 16:01:34 +0900

    2024.02.04 - LeetCode(1732)

--- LeetCode/Easy/1732-find-the-highest-altitude/1732-find-the-highest-altitude.py
+++ LeetCode/Easy/1732-find-the-highest-altitude/1732-find-the-highest-altitude.py
@@ -1,3 +1,4 @@
+# 내 풀이 1
 class Solution:
     def largestAltitude(self, gain: List[int]) -> int:
         answer = [0]
@@ -6,3 +7,15 @@ class Solution:
             answer.append(answer[i] + val)
 
         return max(answer)
+    
+# 내 풀이 2
+class Solution2:
+    def largestAltitude2(self, gain: List[int]) -> int:
+        start = 0
+        answer = 0
+
+        for val in gain:
+            start += val
+            answer = max(start, answer)
+
+        return answer
