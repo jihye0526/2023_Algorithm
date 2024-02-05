ddad5da1c44c87a7f5bb006c382061fb471dab1a
jihye0526 <samtafe97@naver.com>
2024-02-05 21:31:28 +0900

    2024.02.05 - LeetCode(0724)

--- LeetCode/Easy/0724-find-pivot-index/0724-find-pivot-index.py
+++ LeetCode/Easy/0724-find-pivot-index/0724-find-pivot-index.py
@@ -1,3 +1,19 @@
+# 내 풀이 1
+class Solution:
+    def pivotIndex(self, nums: List[int]) -> int:
+        left = 0
+        right = sum(nums) - nums[0]
+        
+        if left == right: return 0
+        
+        for i in range(1, len(nums)):
+            left += nums[i-1]
+            right -= nums[i]
+            if left == right: return i
+            
+        return -1
+    
+# 내 풀이 2 - Runtime : 122 ms, Memory : 17.9 MB
 class Solution:
     def pivotIndex(self, nums: List[int]) -> int:
         left = 0
