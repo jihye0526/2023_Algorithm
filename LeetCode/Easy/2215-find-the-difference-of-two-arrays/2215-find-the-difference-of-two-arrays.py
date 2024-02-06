2409cdb29a554bc976111257ab3d7437563d5f30
jihye0526 <samtafe97@naver.com>
2024-02-06 22:30:40 +0900

    2024.02.06 - LeetCode(2215)

--- LeetCode/Easy/2215-find-the-difference-of-two-arrays/2215-find-the-difference-of-two-arrays.py
+++ LeetCode/Easy/2215-find-the-difference-of-two-arrays/2215-find-the-difference-of-two-arrays.py
@@ -1,5 +1,6 @@
-class Solution:
-    def findDifference(self, nums1: List[int], nums2: List[int]) -> List[List[int]]:
+# 내 풀이
+class Solution1:
+    def findDifference1(self, nums1: List[int], nums2: List[int]) -> List[List[int]]:
         answer = []
 
         nums1_set = set(nums1)
@@ -11,4 +12,14 @@ class Solution:
         answer.append(list(nums2_set -intersection))
 
         return answer
+    
+# 다른 사람 풀이
+class Solution2:
+    def findDifference2(self, nums1: List[int], nums2: List[int]) -> List[List[int]]:
+        set1 = set(nums1)
+        set2 = set(nums2)
         
+        a = list(set1 - set2)
+        b = list(set2 - set1)
+        
+        return [a, b]
