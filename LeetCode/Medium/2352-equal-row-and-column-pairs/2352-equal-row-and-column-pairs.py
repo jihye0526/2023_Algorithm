fc362f82ecbcab586390573dce3e29fd5a84b383
jihye0526 <samtafe97@naver.com>
2024-02-14 23:12:43 +0900

    2024.02.14 - LeetCode(2352)

--- LeetCode/Medium/2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.py
+++ LeetCode/Medium/2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.py
@@ -1,3 +1,4 @@
+# 내 풀이
 import numpy as np
 
 class Solution:
@@ -11,3 +12,19 @@ class Solution:
                     answer += 1
         
         return answer
+    
+# 다른 사람 풀이
+class Solution2:
+    def equalPairs2(self, grid: List[List[int]]) -> int:
+        m = defaultdict(int)
+        cnt = 0
+
+        for row in grid:
+            m[str(row)] += 1
+
+        for i in range(len(grid[0])):
+            col = []
+            for j in range(len(grid)):
+                col.append(grid[j][i])
+            cnt += m[str(col)]
+        return cnt    
