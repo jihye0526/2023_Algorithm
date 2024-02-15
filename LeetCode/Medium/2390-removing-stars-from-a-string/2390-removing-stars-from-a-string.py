3a3fe0533ddbda178fdf68fd8af1227a794d4eb4
jihye0526 <samtafe97@naver.com>
2024-02-15 21:01:30 +0900

    2024.02.15 - LeetCode(2390)

--- LeetCode/Medium/2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.py
+++ LeetCode/Medium/2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.py
@@ -1,3 +1,4 @@
+# 내 풀이
 class Solution:
     def removeStars(self, s: str) -> str:
         arr = []
@@ -9,3 +10,15 @@ class Solution:
                 arr.pop()
 
         return "".join(arr)
+    
+# 다른 사람 풀이
+class Solution2:
+    def removeStars2(self, s: str) -> str:
+        stack = []
+        for ch in s:
+            if ch is '*':
+                stack.pop()
+            else:
+                stack.append(ch)
+
+        return "".join(stack)
